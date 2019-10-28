867ac24ee0e6cce98c4670ca70200123ce5533ff
VectorKappa <vectorkappa@gmail.com>
2019-10-28 15:09:15 +0100

    smol pp
    Co-Authored-By: eRgo35 <michal.czyz35@gmail.com>
    Co-Authored-By: dawidokox1 <jestemdawid213@hotmail.com>

---
+++ VectorKappa/PP/008/008.js
@@ -0,0 +1,4 @@
+
+var imie="Piotr"
+var nazwisko="zwisko"
+console.log(imie + " " + + "a" + nazwisko)
---
+++ VectorKappa/PP/009/009.js
@@ -0,0 +1,16 @@
+var liczba1 = prompt("Podaj liczbę 1")
+var liczba2 = prompt("Podaj liczbę 2")
+var wynik = +liczba1 + +liczba2
+document.write(wynik)
+
+var liczba1 = (2*6)+10
+var liczba2 = liczba1 / 2
+var liczba3 = (liczba1+liczba2)%liczba1
+document.write(liczba1 + "\n" + liczba2 + "\n" + liczba3)
+
+const pi = 3.14159265358979323846264338327950288419716939937510582097494459
+var tekst = "Liczba pi wynosi: "
+document.write(tekst+pi)
+
+var x= 12;
+var y;
