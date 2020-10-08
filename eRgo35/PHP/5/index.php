a0a8710a926a8692022919e0a45610da4b316873
Michał Czyż <michal.czyz35@gmail.com>
2020-10-08 13:23:34 +0200

    10/8/2020 PA

--- eRgo35/PHP/5/index.php
+++ eRgo35/PHP/5/index.php
@@ -1,7 +1,37 @@
 <?php
-    $arr = [];
-    $arr[] = 1;
-    $arr[] = 2;
-    $arr[] = 3;
-    echo $arr[2];
+    $xyz = ["Basia", 25, "kobieta", ["pon", "wt", "śr", "czw", "pt", "sob", "nd"]];
+    echo $xyz[0]." ma ".$xyz[1]." lat, jest ".substr($xyz[2], 0, 6)."ą"." i jej ulubiony dzień tygodnia to ".$xyz[3][4].".";
+?>
+<br><br><br>
+<?php
+    $arr = [5,7,32,57,945,23,64,842,13,63,1,45,73];
+    foreach ($arr as $item) {
+        echo $item.", ";
+    }
+?>
+<br><br><br>
+<?php
+    $ard = sort($arr);
+    foreach ($arr as $it) {
+        echo $it.", ";
+    }
+?>
+<br><br><br>
+<?php
+    $b = ["imie"=>[], "nazwisko"=>[], "wiek"=>[]];
+    $b["imie"] [] = "Anna";
+    $b["nazwisko"] [] = "Maj";
+    $b["wiek"] [] = 25;
+
+    $b["imie"] [] = "Ewa";
+    $b["nazwisko"] [] = "Nowak";
+    $b["wiek"] [] = 29;
+
+    $b["imie"] [] = "Tomasz";
+    $b["nazwisko"] [] = "Kowalski";
+    $b["wiek"] [] = 32;
+
+    echo "<pre>";
+    print_r($b);
+    echo "</pre>";
 ?>
