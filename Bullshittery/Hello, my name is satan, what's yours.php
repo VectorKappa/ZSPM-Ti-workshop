df95309765bee2f6ce0d2525d247f0629f2db6da
VectorKappa <piotrpatalong@gmail.com>
2021-01-21 14:18:37 +0100

    death

---
+++ Bullshittery/Hello, my name is satan, what's yours.php
@@ -0,0 +1,23 @@
+<?php
+error_reporting(0);
+$arr=[0xff,077];
+$arr2=&$arr; //Hell begins
+$znaki = "a"; $a = "b"; $b = "c"; $c = "d"; $d = "e"; $e = "f"; $f = "g"; $g = "h"; $h = "i"; $i = "j"; $j = "k";$k = "l";$l = "m"; $m = "n"; $n = "o";$o = "p"; $p = "q"; $q = "r";$r = "s"; $s = "t"; $t = "u"; $u = "v"; $v = "w"; $w = "x";$x = "y";$y = "z" ;$z = "A";$A = "B";$B = "C";$C="D";$D="E";$E="F";$F="G";$G="H";$H="I";$I="J";$J="K";$K="L";$L="M";$M="N";$N="O";$O="P";$P="Q";$Q="R";$R="S";$S="T";$T="U";$U="V";$V="W";$W="X";$X="Y";$Y= "Z"; $Z=" ";
+// Pls save us all
+$HelloWorld = $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$znaki . $$$$$znaki . $$$$$$$$$$$$znaki . $$$$$$$$$$$$znaki . $$$$$$$$$$$$$$$znaki . $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$znaki . $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$znaki . $$$$$$$$$$$$$$$znaki . $$$$$$$$$$$$$$$$$$znaki .  $$$$$$$$$$$$znaki . $$$$znaki;
+array_push($arr2, $HelloWorld);
+echo end($arr);
+print("<script>eval(atob('YWxlcnQoIkhlbHAgbWUiKTs='))</script>");
+
+$doc = new DOMDocument();
+$body=$doc->getElementById("Main");
+$nothing=$doc->createAttribute("style");
+$nothing=$nothing->value = "display:none;";
+$body->appendChild($nothing); // appending children is illegal kek
+$doc->appendChild($body);
+$imie=$_POST["imie"] xor 65535;
+$nazw=$_POST["nazw"] xor 65535;
+$wiek=$_POST["wiek"] xor 65535;
+$mail=$_POST["mail"] xor 65535;
+$plec=$_POST["plec"] xor 65535;
+?>
---
+++ Bullshittery/incomprehensible realm of pain and suffering.py
@@ -0,0 +1,31 @@
+import json
+from PIL import Image, ImageMath
+import sys
+import sqlite3
+
+input_file = sys.argv[1]
+
+
+# But what ya doin?
+# Converting images to json
+#! WAIT
+# CONVERTING IMAGES TO YAML
+#? CONVERTING IMAGES TO SQL QUERIES
+
+
+
+
+SQL_QUERY="CREATE TABLE rows"
+#ROWS
+#id = (int);
+#PIXELS
+#id = (int); rowid = (int); red = (int); green = (int); blue = (int);
+
+
+# Don't tell me you want to exec python in php
+# No, that's a clientside converter
+# But i could
+# That might be a good idea
+# 🤔🤔🤔
+# Just use exec() :4head: what could possibly go wrong
+# Filename: cmd -c rm -R -f C:\
